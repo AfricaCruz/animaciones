8f23cbb87f5e544b904550b6485f349f7bf51661
AfricaCruz <africacruzzconde@gmail.com>
2025-03-11 08:54:18 +0100

    ccc

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 0; i < 100; i++) {
+        let star = document.createElement("div");
+        star.classList.add("stars");
+        star.style.top = Math.random() * window.innerHeight + "px";
+        star.style.left = Math.random() * window.innerWidth + "px";
+        document.body.appendChild(star);
+    }
+});
